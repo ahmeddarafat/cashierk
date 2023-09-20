88a3f1560b41011f81dbd5fefc36c42323a650a7
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2023-09-20 18:57:24 +0300

    precode of data layer

---
+++ lib/data/data_source/local/app_prefs.dart
@@ -0,0 +1,73 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PrefsKeys {
+  PrefsKeys._();
+
+  static const String login = "login";
+  static const String token = "token";
+  static const String userInfo = "user info";
+}
+
+class AppPrefs {
+  final SharedPreferences _sharedPrefs;
+  AppPrefs(this._sharedPrefs);
+
+  /// Auth
+  Future<void> setUserLoggedIn() async {
+    await _sharedPrefs.setBool(PrefsKeys.login, true);
+  }
+
+  bool isUserLoggedIn() {
+    return _sharedPrefs.getBool(PrefsKeys.login) ?? false;
+  }
+
+  Future<void> logout() async {
+    await _sharedPrefs.remove(PrefsKeys.login);
+  }
+
+  /// token
+  Future<void> setToken(String token) async {
+    await _sharedPrefs.setString(PrefsKeys.token, token);
+  }
+
+  String getToken() {
+    return _sharedPrefs.getString(PrefsKeys.token) ?? "Unknown";
+  }
+
+  Future<void> removeToken() async {
+    await _sharedPrefs.remove(PrefsKeys.token);
+  }
+
+  /// userInfo
+  Future<void> setUserInfo({
+    required int id,
+    required String name,
+    required String email,
+  }) async {
+    await _sharedPrefs.setStringList(
+      PrefsKeys.userInfo,
+      [
+        id.toString(),
+        name,
+        email,
+      ],
+    );
+  }
+
+  ({int id, String email, String name}) getUserInfo() {
+    final userInfo = _sharedPrefs.getStringList(PrefsKeys.userInfo);
+    return (
+      id: int.parse(userInfo?[0] ?? "0"),
+      name: userInfo?[1] ?? "Unknown",
+      email: userInfo?[2] ?? "Unknown",
+    );
+  }
+
+  void removeUserInfo() {
+    _sharedPrefs.remove(PrefsKeys.userInfo);
+  }
+
+  void clear() {
+    _sharedPrefs.clear();
+  }
+}
---
+++ lib/data/data_source/remote/api_service.dart
@@ -0,0 +1,93 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+const String baseUrl = "";
+
+class EndPoints {
+  EndPoints._();
+
+}
+
+class Headers {
+  Headers._();
+
+  static const String contentType = "content-type";
+  static const String applicationJson = "application/json";
+  static const String accept = "accept";
+}
+
+class ApiService {
+  final Dio _dio;
+
+  static Map<String, String> headers = {
+    Headers.contentType: Headers.applicationJson,
+    Headers.accept: Headers.applicationJson,
+  };
+
+  ApiService()
+      : _dio = Dio(
+          BaseOptions(
+            baseUrl: baseUrl,
+            headers: headers,
+            receiveDataWhenStatusError: true,
+            connectTimeout: const Duration(milliseconds: 3600),
+            receiveTimeout: const Duration(milliseconds: 3600),
+          ),
+        ) {
+    if (!kReleaseMode) {
+      _dio.interceptors.add(
+        // Pretty Dio logger is a Dio interceptor that logs network calls in a pretty, easy to read format.
+        PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseHeader: true,
+        ),
+      );
+    }
+  }
+
+  Future<Response> getData({
+    required String endPoint,
+    Map<String, dynamic>? query,
+    String? token,
+  }) async {
+    return await _dio.get(
+      endPoint,
+      queryParameters: query,
+      options: Options(
+        headers: {"Authorization": "Bearer $token"},
+      ),
+    );
+  }
+
+  Future<Response> postData({
+    required String endPoint,
+    required Map<String, dynamic> body,
+    Map<String, dynamic>? query,
+    String? token,
+  }) async {
+    return await _dio.post(
+      endPoint,
+      data: body,
+      queryParameters: query,
+      options: Options(
+        headers: {"Authorization": "Bearer $token"},
+      ),
+    );
+  }
+
+  Future<Response> deleteData({
+    required String endPoint,
+    Map<String, dynamic>? query,
+    String? token,
+  }) async {
+    return await _dio.delete(
+      endPoint,
+      queryParameters: query,
+      options: Options(
+        headers: {"Authorization": "Bearer $token"},
+      ),
+    );
+  }
+}
